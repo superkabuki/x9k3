2cbad6b6acbee1243bd1e988e538d3c7d2d2805c
Super Kabuki <a@iodisco.com>
2025-06-10 12:23:22 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
     platforms="all",
     install_requires=[
         "threefive >= 3.0.53",
-        "m3ufu >= 0.0.83",
+        "m3ufu >= 0.0.93",
     ],
     classifiers=[
         "License :: OSI Approved :: MIT License",
