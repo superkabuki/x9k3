a8146ae6cf53a94f817ee5f41812d723dddcc00e
Super Kabuki <a@iodisco.com>
2025-07-05 17:05:39 -0400

    sliding window

--- x9k3/__init__.py
+++ x9k3/__init__.py
@@ -1,7 +1,8 @@
 """
-x9k3.__init__.py
+x9k3/__init__.py
 """
 
-from .x9k3 import X9K3,  SegmentData, SlidingWindow, Timer, cli, version
+from .x9k3 import X9K3, SegmentData, Timer, cli, version
 from .argue import argue
 from .scte35 import SCTE35
+from .sliding import SlidingWindow
