3d7aefb6390e2051d06dfdc1697a2081a832552c
Super Kabuki <a@iodisco.com>
2025-07-17 13:14:10 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     scripts=["bin/x9k3"],
     platforms="all",
     install_requires=[
-        "threefive >= 3.0.55",
+        "threefive >= 3.0.57",
         "m3ufu >= 0.0.95",
     ],
     classifiers=[
