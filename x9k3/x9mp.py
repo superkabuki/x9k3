3599f2c6caa0807ffcdaa96a923a7e2e0c922dc6
Super Kabuki <a@iodisco.com>
2025-07-17 20:37:19 -0400

    Update x9mp.py

--- x9k3/x9mp.py
+++ x9k3/x9mp.py
@@ -4,46 +4,15 @@ import os
 import sys
 from .abr import ABR
 from m3ufu import M3uFu
-from threefive import reader
+from threefive import reader, blue
 from .x9k3 import X9K3, argue, version
 from .x9k3 import cli as x9cli
 
 
 def do(args):
     """
-    do runs umzz programmatically.
-    Use like this:
-
-    from umzz import do, argue
-
-    args =argue()
-
-    args.input = "/home/a/slow/master.m3u8"
-    args.live = True
-    args.replay = True
-    args.sidecar_file="sidecar.txt"
-    args.output_dir = "out-stuff"
-
-    do(args)
-
-    set any command line options
-    programmatically with args.
-    Here are the defaults returned from argue() .
-
-    input='master.m3u8',
-    continue_m3u8=False,
-    delete=False,
-    live=False,
-    no_discontinuity=False,
-    output_dir='.',
-    program_date_time=False,
-    replay=False,
-    sidecar_file=None,
-    shulga=False,
-    time=2,
-    hls_tag='x_cue',
-    window_size=5,
-
+    do runs x9k3.ABR programmatically.
+ 
     """
     fu = M3uFu()
     if not args.input:
@@ -67,10 +36,9 @@ def ismaster(m3u8):
 def cli():
     """
     cli provides one function call
-    for running shari with command line args
-    Two lines of code gives you a full umzz command line tool.
+    to handle x9k3 with ABR support
 
-     from umzz import cli
+    from x9k3.x9mp import cli
      cli()
 
     """
@@ -78,7 +46,7 @@ def cli():
     if args.version:
         print(version())
         sys.exit()
-    _ = {print(k, "=", v) for k, v in vars(args).items()}
+    _ = {blue(f'{k} = {v}') for k, v in vars(args).items()}
     if ismaster(args.input):
         do(args)
     else:
