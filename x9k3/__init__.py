58754c76b8f4fad74997029788c53db9dc1b1315
Adrian of Doom <a@iodisco.com>
2026-05-20 19:33:09 -0400

    SuperTimer

--- x9k3/__init__.py
+++ x9k3/__init__.py
@@ -5,7 +5,7 @@ x9k3/__init__.py
 
 
 from .abr import ABR
-from .x9k3 import X9K3, SegmentData, Timer, version
+from .x9k3 import X9K3, SegmentData, SuperTimer, version
 from .x9mp import cli
 from .argue import argue
 from .scte35 import SCTE35
